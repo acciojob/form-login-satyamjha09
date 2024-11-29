18fc3582a4c20eb3e746678dbf976e357c59e1df
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-29 13:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function getFormvalue() {
 
+	const form = document.querySelector('form1');
+	
 	const firstName = document.getElementById("fname").value;
 	const lastName = document.getElementById("lname").value;
-
 	const alertMessage = ` ${firstName}  ${lastName}`;
-
 	alert(alertMessage)
-    
-
 }
